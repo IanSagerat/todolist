97e1069e5580daf9531497355447805fa0bbce4e
Stilinski <realesdarren@gmail.com>
2024-09-28 16:20:21 +0800

    added one changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['css/main.css', 'js/main.js'],
+            input: ['/resources/css/main.css', '/resources/js/main.js'],
             refresh: true,
         }),
         vue(),
