0a681f9c53d0c0554dc2a53911409c2b808d217f
Stilinski <realesdarren@gmail.com>
2024-09-28 16:23:37 +0800

    added one changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todolist</title>
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="/resources/css/main.css">
 </head>
 <body>
     <div id="app"></div>
-    <script type="module" src="js/main.js"></script>
+    <script type="module" src="/resources/js/main.js"></script>
 </body>
 </html>
