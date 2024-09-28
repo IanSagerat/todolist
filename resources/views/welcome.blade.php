700fac82cb6b8b7157ff373cfa843dcd32425cbf
Stilinski <realesdarren@gmail.com>
2024-09-28 16:58:02 +0800

    added two changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todolist</title>
-    <link rel="stylesheet" href="{{ vite('resources/css/main.css') }}">
+    {{-- <link rel="stylesheet" href="{{ vite('resources/css/main.css') }}"> --}}
+    @vite(['resources/css/main.css', 'resources/js/main.js'])
 </head>
 <body>
     <div id="app"></div>
-    <script type="module" src="{{ vite('resources/js/main.js') }}"></script>
+    {{-- <script type="module" src="{{ vite('resources/js/main.js') }}"></script> --}}
 </body>
 </html>
